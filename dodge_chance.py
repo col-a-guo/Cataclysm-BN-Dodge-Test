6a5f54cdcfbe2f4bdb2b77ba28b0d2766e3f825f
Collin A Guo <bunjuku8@gmail.com>
2022-06-09 17:54:59 -0700

    fixed bugs, tweaked range, added numerator/added num scaling

--- dodge_chance.py
+++ dodge_chance.py
@@ -4,9 +4,7 @@ def dodge_roll(numer, add, excess, subtract, powernum, powerdenom):
     else:
         return numer/(add+((excess-subtract)**powernum)**(1/powerdenom))
 
-def plunge_bench_error_2_5_1(powernum, powerdenom):
-    numer = 2
-    add = 5
+def plunge_bench_error_2_5_1(numer, add, powernum, powerdenom):
     subtract = 1
     old_roll = 0
     new_roll = dodge_roll(numer, add, 6, subtract, powernum, powerdenom)
@@ -31,15 +29,21 @@ def plunge_bench_error_2_5_1(powernum, powerdenom):
             if new_roll > .25 and new_roll-0.25 < current_2_3_error:
                 current_2_3_error = 1+abs(new_roll-0.25)
         elif i == 5:
-            if .07 < new_roll < .13:
+            if .06 < new_roll < .14:
                 bench_error *= 1
             else:
-                bench_error*=1+min(abs(new_roll-.07)/.07, (abs(new_roll-.13))/.13)
+                # print("new best 5")
+                # print(new_roll)
+                # print(1+min((abs(new_roll-.06))/.06, (abs(new_roll-.14))/.14))
+                bench_error*=1+min((abs(new_roll-.06))/.06, (abs(new_roll-.14))/.14)
         elif i == 10:
             if .02 < new_roll < .05:
-                bench_error *= 1
+                bench_error*=1
             else:
-                bench_error*=1+min(abs(new_roll-.05)/.05, (abs(new_roll-.02))/.02)
+                # print("new best 10")
+                # print(new_roll)
+                # print(1+min((abs(new_roll-.05))/.05, (abs(new_roll-.02))/.02))
+                bench_error*=1+min((abs(new_roll-.05))/.05, (abs(new_roll-.02))/.02)
         if new_roll > 1:
             print(new_roll)
             print(numer, add, i, subtract, powernum, powerdenom)
@@ -47,13 +51,14 @@ def plunge_bench_error_2_5_1(powernum, powerdenom):
     return bench_error, current_2_3_error, current_plunge
 
 best_list = [100,0,0]
-for i in range(3, 11):
-    for j in range (3, 11):
-        bench, err2_3, plunge = plunge_bench_error_2_5_1(i*2+1, j*2+1)
-        err =  bench*err2_3*plunge
-        #print(err)
-        if err < best_list[0]:
-            best_list[0] = err
-            best_list[1] = i
-            best_list[2] = j
-            print(f"Best so far: {best_list}, bench {bench}, 2/3 bench {err2_3}, plunge {plunge}")
+for numeradd_i in range (1,10):
+    for i in range(1, 11):
+        for j in range (1, 11):
+            bench, err2_3, plunge = plunge_bench_error_2_5_1(numeradd_i, numeradd_i*2+1, i*2+1, j*2+1)
+            err =  (bench*err2_3)**(1/3)*plunge
+            #print(err)
+            if err < best_list[0]:
+                best_list[0] = err
+                best_list[1] = i
+                best_list[2] = j
+                print(f"Best so far: {best_list}, bench {bench}, 2/3 bench {err2_3}, plunge {plunge}, numeradd {numeradd_i}")
